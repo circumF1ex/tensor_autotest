4d1eec72f7e28734c82d04dfc91a1f00961a1446
DanilSizz <danilsizov66@gmail.com>
2026-02-02 18:52:15 +0300

    Первый тестовый сценарий выполняется полностью. Описаны все необходимые POM и функции для первого сценария. Реализованы все тесты к ним. Необходимо закомментировать тесты.

--- testсase_1/pages/tensor_about_page.py
+++ testсase_1/pages/tensor_about_page.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 class TensorAboutPage(BasePage):
 
     PAGE_ADRESS = "https://tensor.ru/about"
-    WORKING_BLOCK_CSS = ".tensor_ru-Index__block4 a[href*='/about']"
+    WORKING_BLOCK_CSS = "div.tensor_ru-container:nth-child(4)"
 
     def __init__(self, browser):
         super().__init__(browser)
@@ -15,4 +15,9 @@ class TensorAboutPage(BasePage):
     def open(self):
         super().open(self.PAGE_ADRESS)
 
+    def get_working_block(self):
+        return self.find(By.CSS_SELECTOR, self.WORKING_BLOCK_CSS)
+
+    def get_images_from_working_block(self):
+        return self.get_working_block().find_elements(By.TAG_NAME, "IMG")
     
--- testсase_1/tests/test_scenario_1.py
+++ testсase_1/tests/test_scenario_1.py
@@ -2,6 +2,7 @@ import pytest
 from pages.saby_main_page import SabyMainPage
 from pages.saby_contacts_page import SabyContactsPage
 from pages.tensor_main_page import TensorMainPage
+from pages.tensor_about_page import TensorAboutPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -25,10 +26,31 @@ def test_tensorPeopleStrongBlockVisible(browser):
     result = tensorMainPage.people_strong_block_is_visible()
     assert result == True
 
-def test_openTensorPeopleStrongBlockMoreLink(browser):
+def test_tensorOpenPeopleStrongBlockMoreLink(browser):
 
     tensorMainPage = TensorMainPage(browser)
     tensorMainPage.open()
     tensorMainPage.get_people_strong_block_morelink().click()
     WebDriverWait(browser, 10).until(EC.url_contains("tensor.ru/about"))
     assert "tensor.ru/about" in browser.current_url
+
+def test_tensorGetWorkingBlock(browser):
+    tensorAboutPage = TensorAboutPage(browser)
+    tensorAboutPage.open()
+    assert tensorAboutPage.get_working_block() != None
+
+def test_tensorImageSizesInWorkingBlock(browser):
+
+    tensorAboutPage = TensorAboutPage(browser)
+    tensorAboutPage.open()
+    images = tensorAboutPage.get_images_from_working_block()
+
+    img0 = [images[0].size['height'], images[0].size['width']]
+    result = True
+
+    for i in images:
+        if i.size['height'] != img0[0] or i.size['width'] != img0[1]:
+            result = False
+
+    assert result == True
+
